9a9ee6c2a91916f0837efc0454c29efe89d24b47
Ivan <jf561428@gmail.com>
2023-12-08 17:43:44 -0300

    map js

--- js/map.js
+++ js/map.js
@@ -1,3 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.body.classList.add('fade-in');
+
+
+let isZoomed = false;
+let originalTransform = '';
+
+document.getElementById('mapsvg').addEventListener('click', function(event) {
+    var svg = document.getElementById('mapsvg');
+    if (!isZoomed) {
+        var svgRect = svg.getBoundingClientRect();
+        var x = event.clientX - svgRect.left;
+        var y = event.clientY - svgRect.top;
+
+        var scale = 3; 
+        var translateX = -x * (scale - 1);
+        var translateY = -y * (scale - 1);
+
+        originalTransform = svg.style.transform; 
+        svg.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+        isZoomed = true;
+    } 
 });
+
+document.getElementById("mapsvg").addEventListener('dblclick', function(event){
+    var svg = document.getElementById('mapsvg');
+    if(isZoomed){
+        svg.style.transform = originalTransform; 
+        isZoomed = false;
+    }
+})
