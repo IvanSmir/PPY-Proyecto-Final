051df1e9f8b7c1781b90bfc9a5577e7845372152
Ivan <jf561428@gmail.com>
2023-12-08 21:10:06 -0300

    update styles and js

--- js/map.js
+++ js/map.js
@@ -1,33 +1,151 @@
+const APIURL = "https://restcountries.com/v3.1/name/";
+const APIURLID = "https://restcountries.com/v3.1/alpha/";
 
 
 let isZoomed = false;
 let originalTransform = '';
 
-document.getElementById('mapsvg').addEventListener('click', function(event) {
-    var svg = document.getElementById('mapsvg');
+const mapSvg = document.getElementById('mapsvg');
+mapSvg.addEventListener('click', handleMapClick);
+mapSvg.addEventListener('dblclick', handleMapDoubleClick);
+const mapb = document.getElementById('amap')
+mapb.addEventListener('click', handleMapDoubleClick)
+
+function handleMapClick(event) {
     if (!isZoomed) {
-        var svgRect = svg.getBoundingClientRect();
-        var x = event.clientX - svgRect.left;
-        var y = event.clientY - svgRect.top;
+        zoomIn(event);
+    } else {
+        displayModal(event);
+    }
+}
 
-        var scale = 3; 
-        var translateX = -x * (scale - 1);
-        var translateY = -y * (scale - 1);
+function handleMapDoubleClick() {
+    if (isZoomed) {
+        zoomOut();
+    }
+}
 
-        originalTransform = svg.style.transform; 
-        svg.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
-        isZoomed = true;
-    } else{
-        if (event.target.tagName === 'path') { 
-            console.log('Círculo clickeado:', event.target.id);
+function zoomIn(event) {
+    const svgRect = mapSvg.getBoundingClientRect();
+    const x = event.clientX - svgRect.left;
+    const y = event.clientY - svgRect.top;
+
+    const scale = 3;
+    const translateX = -x * (scale - 1);
+    const translateY = -y * (scale - 1);
+
+    originalTransform = mapSvg.style.transform;
+    mapSvg.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+    isZoomed = true;
+}
+
+function zoomOut() {
+    mapSvg.style.transform = originalTransform;
+    isZoomed = false;
+    updateDisplayStyle('infoModal', "none");
+}
+
+function displayModal(event) {
+    const name = getTargetName(event.target);
+    if (event.target.tagName === 'path') {
+        getCountry(name);
+        positionModal(event);
+    }
+}
+
+function getTargetName(target) {
+    return target.id === "" ? target.className.baseVal : target.id;
+}
+
+function positionModal(event) {
+    const modal = document.getElementById('infoModal');
+    const modalWidth = modal.offsetWidth;
+    const modalHeight = modal.offsetHeight;
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const x = event.clientX;
+    const y = event.clientY;
+    modal.style.left = x + modalWidth > windowWidth ? (x - modalWidth - 20) + 'px' : (x + 20) + 'px';
+    modal.style.top = y + modalHeight > windowHeight ? (y - modalHeight - 20) + 'px' : (y + 20) + 'px';
+    updateDisplayStyle('infoModal', 'block');
+}
+
+function updateDisplayStyle(elementId, displayStyle) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.style.display = displayStyle;
+    }
+}
+
+function getCountry(name) {
+    let api = APIURL
+    if(name.length == 2){
+        api = APIURLID
+    }
+    fetch(api + encodeURIComponent(name))
+    .then(response => response.json())
+    .then(data => {
+        if(data.length==1){
+            updateCountryInfo(data[0]);
+        }else{
+            const exactMatch = data.find(country => country.name.common.toLowerCase() === name.toLowerCase());
+            if (exactMatch) {
+                updateCountryInfo(exactMatch);
+            } else {
+                console.log("País no encontrado o múltiples coincidencias.");
+            }
         }
-    }
-});
+        
+    })
+    .catch(error => {
+        console.error("Error al obtener datos del país:", error);
+    });
+}
 
-document.getElementById("mapsvg").addEventListener('dblclick', function(event){
-    var svg = document.getElementById('mapsvg');
-    if(isZoomed){
-        svg.style.transform = originalTransform; 
-        isZoomed = false;
+function updateCountryInfo(countryData) {
+    updateElementText('countryName', countryData.name.common);
+    updateElementSrc('countryFlag', countryData.flags.png);
+    updateElementText('countryCapital', 'Capital: ' + countryData.capital);
+    updateElementText('countryLanguage', 'Lenguaje: ' + Object.values(countryData.languages).join(', '));
+    updateElementText('countryCurrency', 'Moneda: ' + Object.values(countryData.currencies).map(c => c.name).join(', '));
+    fetchWeatherForCapital(countryData.capital);
+}
+
+function fetchWeatherForCapital(capital) {
+    if (!capital) {
+        updateElementText('countryTemperature', 'Capital desconocida');
+        return;
     }
-})
+    const apiKey = '9c3c48dbbfa71dc169ec9ba8c1a40742';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Problema al obtener datos del clima: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(weatherData => {
+            const temperature = weatherData.main?.temp;
+            updateElementText('countryTemperature', 'Temperatura de la Capital: ' + (temperature ? `${temperature} °C` : 'Desconocida'));
+        })
+        .catch(error => {
+            console.error(error);
+            updateElementText('countryTemperature', 'Error al obtener datos del clima');
+        });
+}
+
+function updateElementText(elementId, text) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.textContent = text;
+    }
+}
+
+function updateElementSrc(elementId, src) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.src = src;
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
         var startButton = document.getElementById('start');
         
         startButton.addEventListener('click', function() {
-            document.getElementById("contenido").classList.add('fade-out');
+            document.getElementById('contenido').classList.add('fade-out');
             setTimeout(function() {
                 window.location.href = 'map.html'; 
             }, 1000); 
@@ -11,4 +10,3 @@
     });
 
 
-    
